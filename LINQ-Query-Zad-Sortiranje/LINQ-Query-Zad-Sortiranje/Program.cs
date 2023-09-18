9b57d12eae287b38889389954f27ff6874c3b4cf
Mannequin52 <92926451+Mannequin52@users.noreply.github.com>
2023-09-18 11:08:49 +0200

    Add files via upload

---
+++ LINQ-Query-Zad-Sortiranje/LINQ-Query-Zad-Sortiranje/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Student
+{
+    public int StudentID { get; set; }
+    public string StudentName { get; set; }
+    public int Age { get; set; }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Student[] studentArray = {
+            new Student() { StudentID = 1, StudentName = "John", Age = 18 },
+            new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 },
+            new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 },
+            new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 },
+            new Student() { StudentID = 5, StudentName = "Ron" , Age = 31 },
+            new Student() { StudentID = 6, StudentName = "Chris",  Age = 17 },
+            new Student() { StudentID = 7, StudentName = "Rob" ,Age = 19 },
+        };
+
+        Console.WriteLine();
+
+        var res = from s in studentArray
+                  orderby s.Age descending
+                  select s;
+
+        foreach (var age in res)
+        {
+            Console.WriteLine(age);
+        }
+
+
+        Console.ReadKey();
+
+    }
+}
